aa33a712e41660eb5439c7d181da2bdef1e7c71a
hbnjwin <33817103+hbnjwin@users.noreply.github.com>
2026-06-11 11:29:03 +0800

    complete l1-111~l1-120: wrv-password-init, wrv-table-empty-css, wrv-align-invalid, wrv-token-expire-instant, wrv-axios-no-timeout, wrv-home-route-dead, wrv-i18n-setup-scope, wrv-blob-error-race, wrv-msg-plugin-wrong, wrv-nav-before-auth (qwen & claude)

--- projects/wrv-table-empty-css-qwen/src/components/table-empty/index.vue
+++
@@ -1,8 +0,0 @@
-<template>
-	<img style="width: 230px" src="@/assets/image/structure/table-empty.png" alt="" />
-</template>
-<style>
-.el-table__empty-block {
-	min-height: 250px !important;
-}
-</style>
