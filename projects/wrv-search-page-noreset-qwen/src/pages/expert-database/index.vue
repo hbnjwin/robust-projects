236593eb32d03c50a38b442e38fa49085135297e
hbnjwin <33817103+hbnjwin@users.noreply.github.com>
2026-06-13 11:22:55 +0800

    complete l1-132: wrv-search-page-noreset (qwen & claude)

--- projects/wrv-search-page-noreset-qwen/src/pages/expert-database/index.vue
+++ projects/wrv-search-page-noreset-qwen/src/pages/expert-database/index.vue
@@ -20,11 +20,11 @@
 								m-r-15px
 								placeholder="请选择"
 								clearable
-								@change="getTableList"
+								@change="handleFilterChange"
 							>
 								<t-option v-for="item in docTypeOptions" :key="item.value" :value="item.value" :label="item.label"></t-option>
 							</t-select>
-							<t-input placeholder="请输入内容" style="width: 260px" @change="getTableList" clearable v-model="table.search.keyWord">
+							<t-input placeholder="请输入内容" style="width: 260px" @change="handleFilterChange" clearable v-model="table.search.keyWord">
 								<template #suffixIcon>
 									<i class="iconfont icon-sousuo" text="14px"></i>
 								</template>
@@ -153,6 +153,11 @@ const getTableList = () => {
 const uploadDialogRef = ref(null)
 const handleOpenUploadDialog = () => {
 	uploadDialogRef.value.show()
+}
+
+const handleFilterChange = () => {
+	table.pagination.page = 1
+	getTableList()
 }
 
 const handlePageSizeChange = (newPageSize) => {
